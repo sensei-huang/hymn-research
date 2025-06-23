0ee35885f3f3fd408c3882ffc45e55b522f47214
sensei-huang <80719864+sensei-huang@users.noreply.github.com>
2025-06-23 17:44:57 +1200

    Create songTitles.js

---
+++ songTitles.js
@@ -0,0 +1,25 @@
+var songs = {};
+async function getSongTitle(num) {
+  const url = "https://songbase.life/blue_songbook/"+num;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      songs[num] = "Not found";
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const rtext = await response.text();
+    const reg = /<title>([\s\S]*?)<\/title>/;
+    if(reg.test(rtext)){
+        var txt = document.createElement("textarea");
+        txt.innerHTML = rtext.match(reg)[1];
+        songs[num] = txt.value;
+    }else{
+
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+for(var i = 1; i < 707; i++){
+    getSongTitle(i);
+}
