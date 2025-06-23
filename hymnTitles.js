e8f3a1301212acb86c7222e976b2d885c7d7a498
sensei-huang <80719864+sensei-huang@users.noreply.github.com>
2025-06-23 17:27:15 +1200

    Create hymnTitles.js

---
+++ hymnTitles.js
@@ -0,0 +1,22 @@
+var hymns = [];
+async function getHymnTitle(num) {
+  const url = "https://songbase.life/english_hymnal/"+num;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const rtext = await response.text();
+    const reg = /<title>([\s\S]*?)<\/title>/;
+    if(reg.test(rtext)){
+        var txt = document.createElement("textarea");
+        txt.innerHTML = rtext.match(reg)[1];
+        hymns.push(txt.value);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+for(var i = 1; i < 1348; i++){
+    getHymnTitle(i);
+}
