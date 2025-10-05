627c5f8de5d62c51a47b62bdf6ed9226d6f92beb
sensei-huang <80719864+sensei-huang@users.noreply.github.com>
2025-10-05 21:01:34 +1300

    Create reformat.js

---
+++ reformat.js
@@ -0,0 +1,31 @@
+let json = document.getElementsByClassName("application-container")[0].children[0].getAttribute("data-react-props");
+let parsed = JSON.parse(json);
+let lyrics = parsed.preloaded_song.lyrics;
+let lines = lyrics.split('\n');
+
+const storage = document.createElement('div');
+document.body.append(storage);
+
+for(let i = 0; i < lines.length; i++){
+  if(lines[i][0] == "#"){
+    const words = document.createElement('div'); // Element to store the words
+    words.style.fontFamily = "Georgia, serif"; // Font type
+    words.style.fontSize = "10pt"; // Font size
+    words.style.fontWeight = 'bold'; // Bold
+    words.style.color = "#999999"; // Font colour
+    words.style.whiteSpace = "pre-wrap"; // To preserve the tab
+    words.innerText = "\t"+lines[i].substring(1, lines[i].length);
+    storage.append(words);
+  }
+}
+
+// p { font-family:Georgia, serif; font-size: 10pt;}
+// p { font-family: 'Arial Black', Gadget, sans-serif; font-size: 7pt;}
+
+var range = document.createRange();
+range.selectNode(storage);
+window.getSelection().removeAllRanges(); // clear current selection
+window.getSelection().addRange(range); // to select text
+document.execCommand("copy");
+window.getSelection().removeAllRanges();// to deselect
+storage.remove();
