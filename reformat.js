625a1b7737e7cb1e3b68ba58fd6a65e047415ae3
sensei-huang <80719864+sensei-huang@users.noreply.github.com>
2025-10-06 14:17:37 +1300

    Update reformat.js

--- reformat.js
+++ reformat.js
@@ -8,24 +8,31 @@ document.body.append(storage);
 
 for(let i = 0; i < lines.length; i++){
   const words = document.createElement('div'); // Element to store the words
+  words.style.whiteSpace = "pre-wrap"; // To preserve the tab
   if(lines[i][0] == "#"){
     words.style.fontFamily = "Georgia, serif"; // Font type
     words.style.fontSize = "10pt"; // Font size
     words.style.fontWeight = 'bold'; // Bold
     words.style.color = "#999999"; // Font colour
-    words.style.whiteSpace = "pre-wrap"; // To preserve the tab
     words.innerText = "\t"+lines[i].substring(1, lines[i].length);
     storage.append(words);
   }else{
-    // if(lines[i].substr(0, 2) == "  "){
     if(lines[i].includes("[")){
+      const chords = document.createElement('div'); // Element to store the chords
+      chords.style.whiteSpace = "pre-wrap"; // To preserve the tab
+      chords.style.fontFamily = "'Arial Black', Gadget, sans-serif"; // Font type
+      chords.style.fontSize = "7pt"; // Font size
+      chords.style.color = "#FFFFFF"; // Font colour
       
     }
     words.style.fontFamily = "Georgia, serif"; // Font type
     words.style.fontSize = "10pt"; // Font size
     words.style.color = "#FFFFFF"; // Font colour
-    words.style.whiteSpace = "pre-wrap"; // To preserve the tab
-    words.innerText = "\t\t"+lines[i].substring(2, lines[i].length);
+    if(lines[i].substr(0, 2) == "  "){
+      words.innerText = "\t\t"+lines[i].substring(2, lines[i].length).replaceAll(/\[.*?\]/g, "");
+    }else{
+      words.innerText = "\t"+lines[i].replaceAll(/\[.*?\]/g, "");
+    }
     storage.append(words);
   }
 }
