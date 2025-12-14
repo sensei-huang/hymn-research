a9763f0c45ab4dee5bbe5bc1ef1695b0ac819efd
sensei-huang <80719864+sensei-huang@users.noreply.github.com>
2025-12-14 19:43:45 +1300

    Update chordPlacer.js

--- chordPlacer.js
+++ chordPlacer.js
@@ -3,45 +3,77 @@ let lyrics = song.props.lyrics;
 let lines = lyrics.split("\n");
 let chorusChords = [];
 
-async function extractChords(l, lnum){ // Lines, line number, array to store chords
+function extractChords(lnum){
 		let arr = [];
-		while(l[lnum] !== "" && lnum != l.length){ // Reach end of chorus or stanza block or end of song
-			let cstr = ""; // Store
-			for(let c = 0; c < l[lnum]; c++){ // Go through each character
-				if(l[lnum][c] === "["){ // Detected chord
+		while(lines[lnum] !== "" && lnum < lines.length){ // Reach end of chorus or stanza block or end of song
+			let cstr = ""; // Store lyrics of this line
+			let chordline = []; // Store chords of this line
+			for(let c = 0; c < lines[lnum].length; c++){ // Go through each character
+				if(lines[lnum][c] === "["){ // Detected chord
 					c++; // Skip '['
 					let chord = "";
-					while(l[lnum][c] !== "]"){ // 
-						chord += l[lnum][c];
+					while(lines[lnum][c] !== "]"){ // Store chord (assumes chord does not reach end of line)
+						chord += lines[lnum][c];
 						c++;
 					}
-					let syl = await syllable(cstr);
-					arr.append([chord, syl]);
+					c++; // Skip ']'
+					let syl = syllable(cstr);
+					arr.push([chord, syl]);
 				}else{
-					cstr += l[lnum][c];
+					cstr += lines[lnum][c];
 				}
 			}
+			lnum++;
 		}	
 		return [lnum, arr];
 }
 
-var callback = async function() {
+function readTune(){
 	// Reading through lines
 	for(let i = 0; i < lines.length; i++){
 		if(lines[i].substring(0, 2) === "  "){ // Chorus
 			if(lines[i].includes("[")){ // Has chords
-				let result = await extractChords(lines, i);
+				let result = extractChords(i);
 				i = result[0];
 				chorusChords = result[1];
 			}else{ // Inject chords into 
-
+				
 			}
 		}
 	}
-	
+}
+
+function writeTune(){
 	// Writing through lines
 	for(let i = 0; i < lines.length; i++){
 		if(lines){
+		}
+	}
+}
+
+function placeChords() {
+	let urlParams = new URLSearchParams(window.location.search);
+	let tune = urlParams.has('tune') ? Number(urlParams.get('tune'))+1 : 0; // Assumes url param query is upto-date which it usually is
+	
+	// Reading through lines
+	for(let i = 0; i < lines.length; i++){
+		if(tune == 0){ // Is currently in correct tune block
+			readTune();
+		}
+		if(lines[i].includes("###")){ // Assumes triple hashtag means tune change
+			tune--;
+		}
+	}
+
+	tune = urlParams.has('tune') ? Number(urlParams.get('tune'))+1 : 0; // Reset tune counter
+	
+	// Writing through lines
+	for(let i = 0; i < lines.length; i++){
+		if(tune == 0){ // Is currently in correct tune block
+			writeTune();
+		}
+		if(lines[i].includes("###")){ // Assumes triple hashtag means tune change
+			tune--;
 		}
 	}
 }
@@ -50,13 +82,13 @@ var callback = async function() {
 var js = document.createElement("script");
 js.type = "module";
 js.innerHTML = `
-import * as mod from 'https://esm.sh/cmu-syllable-counter@1.1.2'; 
-async function syllable(word){
-		let res = await mod.getSyllableCount(word);
-		return res.totalSyllableCount;
+import {syllable} from 'https://esm.sh/syllable@5?bundle';
+import syllables from 'https://esm.sh/syllables@2.2.1?bundle'; 
+function syl(word){
+		return syllables(word, { fallbackSyllablesFunction: syllable });
 }
-window.syllable = syllable;
+window.syllable = syl;
 `;
-js.onreadystatechange = callback();
-js.onload = callback();
+js.onreadystatechange = placeChords();
+js.onload = placeChords();
 document.head.appendChild(js);
