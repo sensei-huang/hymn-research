8a3e95966522286eacf4c99891c001c4bed2ae77
sensei-huang <80719864+sensei-huang@users.noreply.github.com>
2025-12-19 22:30:40 +1300

    Update syllable.js

--- syllable.js
+++ syllable.js
@@ -2,8 +2,8 @@ import {syllable} from 'https://esm.sh/syllable@5?bundle';
 import syllables from 'https://esm.sh/syllables@2.2.1?bundle'; 
 window.syl = function(word){
 	// Double \ to stop string from tampering
-	if(word.length <= 2 && !(/\\s*/.test(word))){ // Eliminate small words like "th" from being two syllables
-		return 1;
+	if(/(^|\\s)[^\\s]{1,2}$/.test(word)){ // Eliminate small words like "th" from being two syllables
+		return syllables(word.replace(/(.*(?:^|\\s))[^\\s]{1,2}$/, ""), { fallbackSyllablesFunction: syllable })+1;
 	}else if(/(^|\\s)[wW]$/.test(word)){ // Eliminate w from being three syllables
 		return syllables(word, { fallbackSyllablesFunction: syllable })-2;
 	}else{
