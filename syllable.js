f61cf87dbd973a1552ef0d9203a1ecf20db34300
sensei-huang <80719864+sensei-huang@users.noreply.github.com>
2025-12-19 22:37:14 +1300

    Update syllable.js

--- syllable.js
+++ syllable.js
@@ -3,7 +3,7 @@ import syllables from 'https://esm.sh/syllables@2.2.1?bundle';
 window.syl = function(word){
 	// Double \ to stop string from tampering
 	if(/(^|\\s)[^\\s]{1,2}$/.test(word)){ // Eliminate small words like "th" from being two syllables
-		return syllables(word.replace(/(.*(?:^|\\s))[^\\s]{1,2}$/, ""), { fallbackSyllablesFunction: syllable })+1;
+		return syllables(word.replace(/[^\\s]{1,2}$/, ""), { fallbackSyllablesFunction: syllable })+1;
 	}else if(/(^|\\s)[wW]$/.test(word)){ // Eliminate w from being three syllables
 		return syllables(word, { fallbackSyllablesFunction: syllable })-2;
 	}else{
