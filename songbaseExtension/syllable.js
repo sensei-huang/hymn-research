94ce9478b77983c9f1325b9a5d8d7e14f8761256
sensei-huang <80719864+sensei-huang@users.noreply.github.com>
2025-12-23 19:02:38 +1300

    Update

--- songbaseExtension/syllable.js
+++ songbaseExtension/syllable.js
@@ -3,7 +3,7 @@ import syllables from 'https://cdn.jsdelivr.net/npm/syllables@2.2.1/+esm';
 window.syl = function(word){
 	// Double \ to stop string from tampering
 	if(/(^|\\s)[^\\s]{1,3}$/.test(word)){ // Eliminate words from becoming pronounced as separate letters in CMU dictionary
-		return syllables(word.replace(/[^\\s]{1,3}$/, ""), { fallbackSyllablesFunction: syllable })+syllable(word.match(/[^\\s]{1,4}$/)[0]);
+		return syllables(word.replace(/[^\\s]{1,3}$/, ""), { fallbackSyllablesFunction: syllable })+syllable(word.match(/[^\\s]{1,3}$/)[0]);
 	}else if(/(^|\\s)[wW]$/.test(word)){ // Eliminate w from being three syllables
 		return syllables(word, { fallbackSyllablesFunction: syllable })-2;
 	}else{
