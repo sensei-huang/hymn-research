88fae77eddee8836d88fa1d5ac3c9a37196ba1bd
sensei-huang <80719864+sensei-huang@users.noreply.github.com>
2025-12-23 21:17:43 +1300

    Fix \ problem

--- songbaseExtension/syllable.js
+++ songbaseExtension/syllable.js
@@ -1,10 +1,9 @@
 import {syllable} from 'https://cdn.jsdelivr.net/npm/syllable@5/+esm';
 import syllables from 'https://cdn.jsdelivr.net/npm/syllables@2.2.1/+esm'; 
 window.syl = function(word){
-	// Double \ to stop string from tampering
-	if(/(^|\\s)[^\\s]{1,3}$/.test(word)){ // Eliminate words from becoming pronounced as separate letters in CMU dictionary
-		return syllables(word.replace(/[^\\s]{1,3}$/, ""), { fallbackSyllablesFunction: syllable })+syllable(word.match(/[^\\s]{1,3}$/)[0]);
-	}else if(/(^|\\s)[wW]$/.test(word)){ // Eliminate w from being three syllables
+	if(/(^|\s)[^\s]{1,3}$/.test(word)){ // Eliminate words from becoming pronounced as separate letters in CMU dictionary
+		return syllables(word.replace(/[^\s]{1,3}$/, ""), { fallbackSyllablesFunction: syllable })+syllable(word.match(/[^\s]{1,3}$/)[0]);
+	}else if(/(^|\s)[wW]$/.test(word)){ // Eliminate w from being three syllables
 		return syllables(word, { fallbackSyllablesFunction: syllable })-2;
 	}else{
 		return syllables(word, { fallbackSyllablesFunction: syllable });
