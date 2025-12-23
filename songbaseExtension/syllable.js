16539df53e0e13a68aef696c5027980735e7a986
sensei-huang <80719864+sensei-huang@users.noreply.github.com>
2025-12-23 18:50:43 +1300

    Fix CMU artifacts

--- songbaseExtension/syllable.js
+++ songbaseExtension/syllable.js
@@ -2,8 +2,8 @@ import {syllable} from 'https://cdn.jsdelivr.net/npm/syllable@5/+esm';
 import syllables from 'https://cdn.jsdelivr.net/npm/syllables@2.2.1/+esm'; 
 window.syl = function(word){
 	// Double \ to stop string from tampering
-	if(/(^|\\s)[^\\s]{1,2}$/.test(word)){ // Eliminate small words like "th" from being two syllables
-		return syllables(word.replace(/[^\\s]{1,2}$/, ""), { fallbackSyllablesFunction: syllable })+1;
+	if(/(^|\\s)[^\\s]{1,3}$/.test(word)){ // Eliminate words from becoming pronounced as separate letters in CMU dictionary
+		return syllables(word.replace(/[^\\s]{1,3}$/, ""), { fallbackSyllablesFunction: syllable })+syllable(word.match(/[^\\s]{1,4}$/)[0]);
 	}else if(/(^|\\s)[wW]$/.test(word)){ // Eliminate w from being three syllables
 		return syllables(word, { fallbackSyllablesFunction: syllable })-2;
 	}else{
