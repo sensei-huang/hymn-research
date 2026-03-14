fd935f8f10efd276b7693c63c7aaa6b0a009a79f
sensei-huang <80719864+sensei-huang@users.noreply.github.com>
2026-03-14 17:34:44 +1300

    Create LifeStudyLatex.js

---
+++ LifeStudyLatex.js
@@ -0,0 +1,40 @@
+let para = document.getElementsByClassName("body")[0].children[0].children;
+let latex = "\\documentclass[10pt]{article}\n\\usepackage[a4paper, margin=1cm]{geometry}\n\\usepackage{relsize}\n\\usepackage{fp}\n\\usepackage{adjustbox}\n\n\\begin{document}\n\\begin{center}";
+let scaleFactor = "0.65";
+for(let i = 0; i < para.length; i++){
+    if(para[i].tagName == "P" && para[i].innerText != ""){
+        latex += "\\scalebox{"+scaleFactor+"}{\n\\parbox{\\fpeval{\\textwidth/"+scaleFactor+"}}{\n";
+        latex += para[i].innerText;
+        latex += "\n}\n}\n";
+    }
+    if(para[i].tagName == "H1" || para[i].tagName == "H2" && para[i].innerText != ""){
+        latex += "\\scalebox{"+scaleFactor+"}{\n\\parbox{\\fpeval{\\textwidth/"+scaleFactor+"}}{\n\\textbf{";
+        latex += para[i].innerText;
+        latex += "}\n}\n}\n";
+    }
+}
+latex += "\\end{center}\n\\end{document}";
+
+// Copy text to clipboard
+const storage = document.createElement('div');
+document.body.append(storage);
+storage.innerText = latex;
+var range = document.createRange();
+range.selectNode(storage);
+window.getSelection().removeAllRanges(); // clear current selection
+window.getSelection().addRange(range); // to select text
+document.execCommand("copy");
+window.getSelection().removeAllRanges();// to deselect
+storage.remove();
+/*
+When document.execCommand doesn't work, switch to the next approach:
+const clipboardItem = new ClipboardItem({ 
+  'text/html': new Blob([html], { type: 'text/html' }),
+  'text/plain': new Blob([html], { type: 'text/plain' })
+});
+
+navigator.clipboard.write([clipboardItem])
+  .then(() => console.log("clipboard.write() Ok"))
+  .catch(error => alert(error))
+*/
+alert("Copied latex!");
